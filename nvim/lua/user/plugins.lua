a45084ef6639369712b7f6f6ca34fd24d153bd6c
Cosme Escobedo <comeoes@gmail.com>
2023-01-28 10:06:20 -0600

    init commit

---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,147 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').reset()
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'solid' })
+    end,
+  },
+})
+
+local use = require('packer').use
+
+use('wbthomason/packer.nvim')
+
+use({
+    'Mofiqul/dracula.nvim', 
+    config = function()
+        vim.cmd('colorscheme dracula')
+
+        vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+            fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+            bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+        })
+    end,
+})
+use('tpope/vim-commentary')
+use('tpope/vim-surround')
+use('tpope/vim-eunuch')
+use('tpope/vim-unimpaired')
+use('tpope/vim-sleuth')
+use('tpope/vim-repeat')
+use('sheerun/vim-polyglot')
+use('christoomey/vim-tmux-navigator')
+use('farmergreg/vim-lastplace')
+use('nelstrom/vim-visual-star-search')
+use('jessarcher/vim-heritage')
+use({
+    'whatyouhide/vim-textobj-xmlattr',
+    requires = 'kana/vim-textobj-user',
+})
+use({
+    'airblade/vim-rooter',
+    setup = function()
+        vim.g.rooter_manual_only = 1
+    end,
+    config = function ()
+        vim.cmd('Rooter')
+    end,
+})
+use({
+    'windwp/nvim-autopairs',
+    config = function()
+        require('nvim-autopairs').setup()
+    end,
+})
+use({
+    'karb94/neoscroll.nvim',
+    config = function()
+        require('neoscroll').setup()
+    end,
+})
+use({
+    'famiu/bufdelete.nvim',
+    config = function()
+        vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+    end,
+})
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+      vim.g.splitjoin_trailing_comma = 1
+      vim.g.splitjoin_php_method_chain_full = 1
+  end,
+})
+
+use({
+  'sickill/vim-pasta',
+  config = function()
+      vim.g.pasta_disabled_filetypes = { 'fugitive' }
+  end,
+})
+
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim' },
+  },
+  config = function()
+    require('user.plugins.telescope')
+  end,
+})
+
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user.plugins.nvim-tree')
+  end,
+})
+
+use({
+    'nvim-lualine/lualine.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+        require('user.plugins.lualine')
+    end,
+})
+
+use({
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    after = 'dracula.nvim',
+    config = function()
+      require('user.plugins.bufferline')
+    end,
+ })
+
+
+-- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
+if packer_bootstrap then
+require('packer').sync()
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
